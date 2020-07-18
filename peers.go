3ff540dbef314b2ade8bee4f0c1be3aecf8a4bbd
dongxiem <ljmiaw@163.com>
2020-07-18 19:54:04 +0800

    添加peers

--- peers.go
+++ peers.go
@@ -6,13 +6,13 @@ import pb "carrotCache/gocachepb"
 
 // PeerPicker is the interface that must be implemented to locate
 // the peer that owns a specific key.
-// 该方法用于根据传入的 key 选择相应节点 PeerGetter
+// PeerPicker ：该方法定义根据传入的 key 选择相应节点 PeerGetter
 type PeerPicker interface {
 	PickPeer(key string) (peer PeerGetter, ok bool)
 }
 
 // PeerGetter is the interface that must be implemented by a peer.
-// 该法用于从对应 group 查找缓存值。
+// PeerGetter ：该方法用于从对应 group 查找缓存值。
 type PeerGetter interface {
 	// 第二个参数使用 gocachepb.pb.go 中的数据类型
 	Get(in *pb.Request, out *pb.Response) error
